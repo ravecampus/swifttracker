2442652ec43449745e5534a32bf8d0b2bbcdb82b
Lavwin Campollo <rave_campus@hotmail.com>
2016-07-10 20:57:22 +0800

    with authencation

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,29 +3,7 @@ from django.contrib.auth.models import User
 from .models import Profile, Project, WeeklyReport
 from django import forms
 	
-# class ValidationSignUp1(forms.Form):
-  
-#     username = forms.CharField()
-#     email = forms.EmailField()
-#     password = forms.CharField(widget=forms.PasswordInput)
-#     confirm_password = forms.CharField(widget=forms.PasswordInput)
-#     def clean_username(self):
-#         username = self.cleaned_data.get('username')
-#         if username and User.objects.filter(username=username).count():
-#             raise forms.ValidationError(u"Duplicate Username!, You can't proceed!")    
-#         return username
 
-#     def clean_email(self):
-#         email = self.cleaned_data.get('email')
-#         return email
-
-#     def clean(self):
-#         password = self.cleaned_data.get('password')
-#         confirm_password = self.cleaned_data.get('confirm_password')
-#         if password and password != confirm_password:
-#             raise forms.ValidationError('Unmatch Password')
-#         return self.cleaned_data
-    
 class ValidationSignUp(forms.ModelForm):
     email  = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -73,53 +51,14 @@ class Login(forms.Form):
             self.user_cache = user
 
 
-# class EditForm(forms.Form):
-#     POSITION = (
-#         ('TRAINEE','trainee'),
-#         ('DEVELOPER','developer'),
-#         ('DESIGNER','designer'))
-
-#     first_name = forms.CharField(max_length =200)
-#     last_name = forms.CharField(max_length=200)
-#     position = forms.CharField(widget=forms.Select(choices= POSITION))
-#     birthdate = forms.DateField(widget=forms.TextInput(attrs={
-#         'id':'datepicker',
-#         'data-date-format':'yyyy-mm-dd',
-#         }),input_formats=['%Y-%m-%d'])
-#     phone = forms.CharField(max_length=200)
-#     address =forms.CharField(max_length=200)
-
-#     def __init__(self, *args, **kwargs):
-#         self.request = kwargs.pop('request', None)
-#         return super(EditForm, self).__init__(*args, **kwargs)
-
-    
-#     def save(self, force_insert=False, force_update=False, commit=True):
-#         instance = super(EditForm, self).save(commit=False)
-
-#         if commit:
-#             instance.last_name = self.cleaned_data['last_name']
-#             intance.first_name = self.cleaned_data['first_name']
-            
-#         return instance 
-
 class EditForm(forms.ModelForm):
-    # POSITION = (
-    #     ('TRAINEE','trainee'),
-    #     ('DEVELOPER','developer'),
-    #     ('DESIGNER','designer'))
-
     first_name = forms.CharField(max_length =200)
     last_name = forms.CharField(max_length=200)
-    # position = forms.CharField(widget=forms.Select(choices= POSITION))
     birthdate = forms.DateField(widget=forms.TextInput(attrs={
         'id':'datepicker',
         'data-date-format':'yyyy-mm-dd',
         }),input_formats=['%Y-%m-%d'])
-    # phone = forms.CharField(max_length=200)
-    # address =forms.CharField(max_length=200)
-
-
+    
     class Meta:
         model = Profile
         fields = {
@@ -131,24 +70,7 @@ class EditForm(forms.ModelForm):
         'address'}
 
    
-# class WeeklyReports(forms.Form):
-#     title = forms.CharField(max_length = 2000)
-#     date_track = forms.DateField(label='Date', widget=forms.TextInput(attrs={
-#         'id':'datepicker',
-#         'data-date-format':'yyyy-mm-dd'
-#         }),input_formats=['%Y-%m-%d'])
-#     question1 = forms.CharField(label='What I did?:', widget=forms.Textarea)
-#     question2 = forms.CharField(label='What to do?:', widget=forms.Textarea)
-#     question3 = forms.CharField(label='Issues/Blocker:', widget=forms.Textarea)
-#     time_track = forms.FloatField(label='weekly hours')
-
-#     def clean_date_track(self):
-#         date_track = self.cleaned_data.get('date_track')
-#         return date_track
-
-
 class WeeklyReports(forms.ModelForm):
-
     date_track = forms.DateField(label='Date', widget=forms.TextInput(attrs={
         'id':'datepicker',
         'data-date-format':'yyyy-mm-dd'
@@ -159,8 +81,6 @@ class WeeklyReports(forms.ModelForm):
     time_track = forms.FloatField(label='weekly hours')
 
     def __init__(self, *args, **kwargs):
-        # {'user': user, }
-        # (requestPOST)
         self.user = kwargs.pop('user', None)
         self.project = kwargs.pop('earvin',None)
         return super(WeeklyReports, self).__init__(*args, **kwargs)
@@ -180,7 +100,6 @@ class WeeklyReports(forms.ModelForm):
 
 
 class WeeklyReportsEdit(forms.ModelForm):
-
     title = forms.CharField(max_length = 2000)
     date_track = forms.DateField(label='Date', widget=forms.TextInput(attrs={
         'id':'datepicker',
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from .forms import ValidationSignUp, Login, EditForm, WeeklyReports, WeeklyReportsEdit
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .models import Profile, Project, WeeklyReport
 from django.contrib.auth import login, logout
 from django.http import HttpResponseRedirect
@@ -8,12 +9,17 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.conf import settings
 import datetime
+import calendar
+
 # class based view
 from django.views.generic import TemplateView
 
-# function based
-# def home_view(request):
-#     return render(request, 'pages/home.html',{})
+class LoggedInMixin(object):
+    @method_decorator(login_required(login_url='login'))
+    def dispatch(self, *args, **kwargs):
+
+        return super(LoggedInMixin, self).dispatch(*args, **kwargs)
+
 
 #class based
 class HomeView(TemplateView):
@@ -21,24 +27,7 @@ class HomeView(TemplateView):
     context = {}
     def get(self, *args, **kwargs):
         return render(self.request, self.template_name, self.context)
-# def signup_view1(request):
-#     if request.method == "POST":
-#         form = ValidationSignUp(request.POST)
-#         if form.is_valid():
-#             info = request.POST
-        
-#             user = User.objects.create(
-#                 username = info['username'],
-#                 email = info['email'])
-#             user.set_password(info['password'])
-#             user.save()
-#             profile = Profile.objects.create(user=user, phone='', address='')
-#             print(request.POST)
-#             return HttpResponseRedirect(reverse('login'))
-#         else:
-#             return render(request, "pages/signup.html",{'form':form})
-#     form = ValidationSignUp
-#     return render(request, "pages/signup.html",{'form':form})
+
 
 # class based 
 class SignupView(TemplateView):
@@ -57,21 +46,6 @@ class SignupView(TemplateView):
 
         self.context['form'] = form
         return render(self.request, self.template_name, self.context)
-
-#function based
-
-# function based
-# def login_view(request):
-#     form = Login()
-#     if request.method == 'POST':
-#         form = Login(request.POST)
-#         if form.is_valid():
-#             login(request, form.user_cache)
-#             return HttpResponseRedirect(reverse('dashboard'))
-#         else:
-#             return render(request,'pages/login.html',{'form':form})
-#     else:
-#         return render(request, 'pages/login.html',{'form':form})
 
 
 #class based
@@ -94,32 +68,9 @@ class LoginView(TemplateView):
         self.context['form'] = form
         return render(self.request, self.template_name, self.context)
 
-#function based
-
-# @login_required(login_url='login')
-# def dashboard_view(request):
-#     if request.user.is_authenticated():
-#         info = Profile.objects.get(user=request.user)
-#         proj = Project.objects.filter(username=request.user)
-
-#         if info.birthdate:
-#              now = datetime.datetime.now()    
-#              # profilebd = info.birthdate.year
-#              age = int((datetime.date.today() - info.birthdate).days / 365.25  )
-
-#              context = {'info':info,'proj':proj,'age':age}  
-#              return render(request,'pages/dashboard.html', context)
-#         else:
-#             context = {'info':info,'proj':proj}
-        
-#             return render(request, 'pages/dashboard.html', context)
-#     else:
-#         return HttpResponseRedirect(reverse('login'))
-
 
 #class based
-
-class DashboardView(TemplateView):
+class DashboardView(LoggedInMixin, TemplateView):
    
     template_name = 'pages/dashboard.html'
     context = {}
@@ -139,52 +90,19 @@ class DashboardView(TemplateView):
         return render(self.request, self.template_name, self.context)
 
     def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated():
-            info = Profile.objects.get(user=self.request.user)
-            projects = Project.objects.filter(username=self.request.user)
+        # if self.request.user.is_authenticated():
+        info = Profile.objects.get(user=self.request.user)
+        projects = Project.objects.filter(username=self.request.user)
 
-            self.context['info'] = info
-            self.context['projects'] = projects
-            return render(self.request, self.template_name, self.context)
-        else:
-            return HttpResponseRedirect(reverse('login'))
+        self.context['info'] = info
+        self.context['projects'] = projects
+        return render(self.request, self.template_name, self.context)
+        # else:
+        #     return HttpResponseRedirect(reverse('login'))
     
-#function based
-
-# @login_required(login_url='login')
-# def edit_profile_view(request):
-#     profile = Profile.objects.get(user=request.user)
-#     form = EditForm(initial={
-#         'first_name': request.user.first_name,
-#         'last_name': request.user.last_name,
-#         'birthdate': profile.birthdate,
-#         'position' :profile.position,
-#         'phone':profile.phone,
-#         'address':profile.address})
-    
-#     if request.method == 'POST':
-#         form = EditForm(request.POST)
-#         if form.is_valid():
-
-#             info = request.POST
-#             user = User.objects.filter(username=request.user)
-#             user.update(last_name = info['last_name'])
-#             user.update(first_name = info['first_name'])
-        
-#             profile = Profile.objects.filter(user=request.user)
-#             profile.update(
-#                 position = info['position'],
-#                 birthdate = info['birthdate'],
-#                 phone = info['phone'],
-#                 address = info['address']) 
-#             return HttpResponseRedirect(reverse('dashboard'))
-#         else:
-#             return render(request, 'pages/edit_profile.html', {'form':form})
-
-#     return render(request, 'pages/edit_profile.html', {'form':form})
 
 #class based
-class EditProfileView(TemplateView):
+class EditProfileView(LoggedInMixin, TemplateView):
     template_name = 'pages/edit_profile.html'
     context = {}
 
@@ -215,16 +133,9 @@ class EditProfileView(TemplateView):
             self.context['form'] = form
         return render(self.request, self.template_name, self.context)
 
-#funtion based
-# @login_required(login_url='login')
-# def project_view(request, project_id): 
-#     data = Project.objects.get(id = project_id)
-#     reports = WeeklyReport.objects.filter(project_name =data, user =request.user).order_by('-id')
-#     return render(request ,'pages/projects.html',{'data':data, 'reports':reports })
-
 
 #class based
-class ProjectView(TemplateView):
+class ProjectView(LoggedInMixin, TemplateView):
     template_name = 'pages/projects.html'
     context = {}
 
@@ -235,33 +146,9 @@ class ProjectView(TemplateView):
         self.context['reports'] = reports
         return render(self.request, self.template_name, self.context)
 
-#function based 
-
-# @login_required(login_url='login')
-# def  add_report_view(request, project_id):
-#     form = WeeklyReports()
-#     if request.method == 'POST':
-#         form = WeeklyReports(request.POST)
-#         if form.is_valid():
-#             info = request.POST
-#             projects = Project.objects.get(id=project_id, username=request.user)
-#             reports = WeeklyReport.objects.create(
-#                 project_name = projects, 
-#                 user = request.user,
-#                 title = info['title'],
-#                 date_track = info['date_track'],
-#                 question1 = info['question1'],
-#                 question2 = info['question2'],
-#                 question3 = info['question3'],
-#                 time_track = info['time_track']
-#                 )
-#             return HttpResponseRedirect(reverse('project_detail', kwargs={'project_id':project_id}))
-#         else:
-#             return render(request, 'pages/add_report.html', {'form':form})
-#     return render(request, 'pages/add_report.html',{'form':form})
 
 #class based
-class AddReportView(TemplateView):
+class AddReportView(LoggedInMixin, TemplateView):
     template_name = 'pages/add_report.html'
     context = {}
 
@@ -282,39 +169,9 @@ class AddReportView(TemplateView):
             self.context['form'] = form
         return render(self.request, self.template_name, self.context)
 
-#function based
-
-# @login_required(login_url='login')
-# def edit_report_view(request,project_id ,report_id):
-
-#     project = WeeklyReport.objects.get(user=request.user ,id=report_id)
-#     form = WeeklyReportsEdit(initial={
-#         'title':project.title,
-#         'date_track':project.date_track,
-#         'question1':project.question1,
-#         'question2':project.question2,
-#         'question3':project.question3,
-#         'time_track':project.time_track
-#         })
-#     if request.method == 'POST':
-#         form = WeeklyReportsEdit(request.POST)
-#         if form.is_valid():
-#             info = request.POST
-#             reports = WeeklyReport.objects.filter(id=report_id)
-#             reports.update(title = project.title,
-#             date_track = info['date_track'],
-#             question1 = info['question1'],
-#             question2 = info['question2'],
-#             question3 = info['question3'],
-#             time_track = info['time_track'])
-#         return HttpResponseRedirect(reverse('project_detail', kwargs={'project_id':project_id}))
-#     else:
-#         return render(request, 'pages/edit_report.html',{'form':form})
-
-#     return render(request, 'pages/edit_report.html',{'form':form})
 
 #class based
-class EditReportView(TemplateView):
+class EditReportView(LoggedInMixin, TemplateView):
     template_name = 'pages/edit_report.html'
     context = {}
 
@@ -334,11 +191,6 @@ class EditReportView(TemplateView):
             self.context['form'] = form
         return render(self.request, self.template_name, self.context)
 
-#function base
-
-# def logout_view(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('login'))
 
 #class based
 class LogoutView(TemplateView):
